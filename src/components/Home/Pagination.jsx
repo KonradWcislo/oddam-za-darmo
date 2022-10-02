da7983afa005d010b54eebaf354ce1e7f07b3a0d
KonradWcislo <konrad.wcislo95@gmail.com>
2022-10-02 17:59:52 +0200

    upgrade

---
+++ src/components/Home/Pagination.jsx
@@ -0,0 +1,16 @@
+export const Pagination = ({ pageCount, currentPage, onPageChange }) => {
+	const handlePageClick = (e, page) => {
+		console.log(`Selected Pagination ${page}`)
+		e.preventDefault()
+		onPageChange(page)
+	}
+
+	return (
+		<ul>
+			{pageCount > 1 &&
+				Array.from(Array(pageCount).keys()).map(n => (
+					<li onClick={e => handlePageClick(e, n)}>{n + 1}</li>
+				))}
+		</ul>
+	)
+}
